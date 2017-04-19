c53aea9cf3d46353935e1f39939e1907022fe380
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-04-19 16:51:12 +0300

    build: web ui packaging

--- build.go
+++ build.go
@@ -7,15 +7,52 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
+	log "github.com/Sirupsen/logrus"
+	"go/format"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
 
-	log "github.com/Sirupsen/logrus"
+const (
+	target = "github.com/filemaps/filemaps-backend/cmd/filemaps"
+)
+
+var tmpl = template.Must(template.New("assets").Parse(`package httpd
+
+import "encoding/base64"
+
+func GetAssets() map[string][]byte {
+	var assets = make(map[string][]byte, {{.Assets | len}})
+{{range $asset := .Assets}}
+	assets["{{$asset.Name}}"], _ = base64.StdEncoding.DecodeString("{{$asset.Content}}"){{end}}
+	return assets
+}
+`))
+
+type asset struct {
+	Name    string
+	Content string
+}
+
+type tmplVars struct {
+	Assets []asset
+}
+
+var (
+	assets []asset
 )
 
 func main() {
 	log.Info("Building and installing File Maps")
-	target := "github.com/filemaps/filemaps-backend/cmd/filemaps"
+	packageWebUI("filemaps-webui/src/", "pkg/httpd/webui.go")
 	run("go", "install", target)
 }
 
@@ -30,4 +67,64 @@ func run(cmd string, args ...string) {
 			"args": args,
 		}).Error(err)
 	}
+}
+
+func getWalkFunc(base string) filepath.WalkFunc {
+	return func(name string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if strings.HasPrefix(filepath.Base(name), ".") {
+			// ignore files beginning with dot
+			return nil
+		}
+
+		if info.Mode().IsRegular() {
+			f, err := os.Open(name)
+			if err != nil {
+				return err
+			}
+
+			// read file contents and gzip it to buffer
+			var buf bytes.Buffer
+			g := gzip.NewWriter(&buf)
+			io.Copy(g, f)
+			f.Close()
+			g.Flush()
+			g.Close()
+
+			// create asset struct and append it to vars
+			name, _ = filepath.Rel(base, name)
+			assets = append(assets, asset{
+				Name:    filepath.ToSlash(name),
+				Content: base64.StdEncoding.EncodeToString(buf.Bytes()),
+			})
+		}
+		return nil
+	}
+}
+
+// packageWebUI packages Web UI files into a single go file.
+// All files are gzipped and base64 encoded into static strings.
+func packageWebUI(path string, out string) {
+	log.Info("Packaging Web UI")
+	filepath.Walk(path, getWalkFunc(path))
+
+	var buf bytes.Buffer
+	tmpl.Execute(&buf, tmplVars{
+		Assets: assets,
+	})
+
+	bs, err := format.Source(buf.Bytes())
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(out, bs, 0644)
+	if err != nil {
+		panic(err)
+	}
+	log.WithFields(log.Fields{
+		"output": out,
+	}).Info("Web UI packaged")
 }
