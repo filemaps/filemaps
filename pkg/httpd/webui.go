2883a2cd1f827089fc99946dc5563f14e24c5b15
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-04-25 21:03:55 +0300

    stub webui.go for testing

---
+++ pkg/httpd/webui.go
@@ -0,0 +1,8 @@
+package httpd
+
+// GetAssets returns static web UI assets.
+// This file is overwritten when running `go run build.go`
+func GetAssets() map[string][]byte {
+	var assets map[string][]byte
+	return assets
+}
