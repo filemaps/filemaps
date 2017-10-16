1bddb735582bac42b0431e632303ac226af7d03c
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-10-17 02:59:48 +0300

    WebUI v0.1.1

--- build.go
+++ build.go
@@ -27,7 +27,7 @@ import (
 const (
 	target       = "github.com/filemaps/filemaps/cmd/filemaps"
 	webuiPath    = "filemaps-webui/"
-	webuiVersion = "0.1.0"
+	webuiVersion = "0.1.1"
 )
 
 var tmpl = template.Must(template.New("assets").Parse(`package httpd
